a9c87feec4a57e4deb53247ae2170f9f4e91beac
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:38:26 +0530

    Update Season.java

--- methods/level1/Season.java
+++ methods/level1/Season.java
@@ -2,22 +2,30 @@
 Hint => Spring Season is from March 20 to June 20. Write a Method to check for Spring season and return a boolean true or false  */
 import java.util.Scanner;
 
-class Season{
-    void Seasoncheck(int date,int month){
-        if(date>=20 && month==3 || month==4 || month==5 || month==6 && date<=20){
+class Season {
+
+    // Method to check whether the given date falls in Spring season
+    void Seasoncheck(int date, int month) {
+        if (date >= 20 && month == 3 || month == 4 || month == 5 || month == 6 && date <= 20) {
             System.out.println("It is Spring");
-        }
-        else{
+        } else {
             System.out.println("It is not Spring");
         }
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
         
-        int date=sc.nextInt();
-        int month=sc.nextInt();
-        Season obj=new Season();
+        // Read date and month
+        int date = sc.nextInt();
+        int month = sc.nextInt();
+
+        // Create object of Season class
+        Season obj = new Season();
         
-        obj.Seasoncheck(date,month);
+        // Call method to check season
+        obj.Seasoncheck(date, month);
     }
 }
