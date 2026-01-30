4c906a1ebde93221eddd28b7e5b84d9cb201f048
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:22:17 +0530

    Update SamMark.java

--- javaprogrammingelements/level1/SamMark.java
+++ javaprogrammingelements/level1/SamMark.java
@@ -3,13 +3,18 @@ Sam’s mark in Maths is 94, Physics is 95 and Chemistry is 96 out of 100. Find 
 I/P => NONE
 O/P => Sam’s average mark in PCM is ___ 
 */
-class SamMark{
-    public static void main(String[] args){
-        int Maths=94;
-        int Physics=95;
-        int Chemistry=96;
+class SamMark {
+    public static void main(String[] args) {
+
+        // Marks obtained in subjects
+        int Maths = 94;
+        int Physics = 95;
+        int Chemistry = 96;
         
-        double average=(Maths+Physics+Chemistry)/3.0;
-        System.out.println("Sam average mark in PCM is "+average);
+        // Calculate average marks
+        double average = (Maths + Physics + Chemistry) / 3.0;
+
+        // Display the average marks
+        System.out.println("Sam average mark in PCM is " + average);
     }
 }
