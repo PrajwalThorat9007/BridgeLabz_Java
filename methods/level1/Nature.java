c07795e166687b0627f5bdde0bd0cebdc48aba77
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:34:45 +0530

    Update Nature.java

--- methods/level1/Nature.java
+++ methods/level1/Nature.java
@@ -2,24 +2,31 @@
 Hint => Get integer input from the user. Write a Method to return -1 for negative number, 1 for positive number and 0 if number is zero */
 import java.util.Scanner;
 
-class Nature{
-    void Nature(int num){
-        if(num>0){
+class Nature {
+
+    // Method to check whether the number is positive, negative, or zero
+    void Nature(int num) {
+        if (num > 0) {
             System.out.println("Positive");
-        }
-        else if(num<0){
+        } else if (num < 0) {
             System.out.println("Negative");
-        }
-        else{
+        } else {
             System.out.println("Zero");
         }
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
         
-        int num=sc.nextInt();
-        Nature obj=new Nature();
+        // Read the number
+        int num = sc.nextInt();
+
+        // Create object of Nature class
+        Nature obj = new Nature();
         
+        // Call method to check nature of the number
         obj.Nature(num);
     }
 }
