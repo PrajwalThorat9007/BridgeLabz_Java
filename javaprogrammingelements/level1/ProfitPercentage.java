63bdd475394b753749ff924ac881818fbe3570cb
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:20:29 +0530

    Update ProfitPercentage.java

--- javaprogrammingelements/level1/ProfitPercentage.java
+++ javaprogrammingelements/level1/ProfitPercentage.java
@@ -9,14 +9,31 @@ O/P =>
 The Cost Price is INR ___ and Selling Price is INR ___
 The Profit is INR ___ and the Profit Percentage is ___
 */
-class ProfitPercentage{
-    public static void main(String[] args){
-        int costPrice=129;
-        int sellPrice=191;
-        int profit=sellPrice-costPrice;
+class ProfitPercentage {
+    public static void main(String[] args) {
+
+        // Cost price of the item
+        int costPrice = 129;
+
+        // Selling price of the item
+        int sellPrice = 191;
+
+        // Calculate profit
+        int profit = sellPrice - costPrice;
         
-        int profitPercent=(profit*100)/costPrice;
-        System.out.println("The Cost Price is INR "+costPrice+ " and Selling Price is INR "+sellPrice);
-        System.out.println("The Profit is INR "+profit+"and the Profit Percentage is "+profitPercent);
+        // Calculate profit percentage
+        int profitPercent = (profit * 100) / costPrice;
+
+        // Display cost price and selling price
+        System.out.println(
+            "The Cost Price is INR " + costPrice +
+            " and Selling Price is INR " + sellPrice
+        );
+
+        // Display profit and profit percentage
+        System.out.println(
+            "The Profit is INR " + profit +
+            "and the Profit Percentage is " + profitPercent
+        );
     }
 }
