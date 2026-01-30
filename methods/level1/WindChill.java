d5247b30d81f5d9eb0ac30fd1f497766c52812ce
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:40:21 +0530

    Update WindChill.java

--- methods/level1/WindChill.java
+++ methods/level1/WindChill.java
@@ -6,20 +6,31 @@ public double calculateWindChill(double temperature, double windSpeed)
  */
 import java.util.Scanner;
 
-class WindChill{
-    public double calculateWindChill(double temperature, double windSpeed){
-        double windChill=35.74+0.6215* temperature+(0.4275* temperature-35.75)*Math.pow(windSpeed,0.16);
+class WindChill {
+
+    // Method to calculate wind chill based on temperature and wind speed
+    public double calculateWindChill(double temperature, double windSpeed) {
+        double windChill = 35.74 + 0.6215 * temperature +
+                (0.4275 * temperature - 35.75) * Math.pow(windSpeed, 0.16);
         return windChill;
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
         
-        double temprature=sc.nextDouble();
-        double windSpeed=sc.nextDouble();
+        // Read temperature and wind speed
+        double temprature = sc.nextDouble();
+        double windSpeed = sc.nextDouble();
         
-        WindChill obj=new WindChill();
-        double windChill=obj.calculateWindChill(temprature,windSpeed);
+        // Create object of WindChill class
+        WindChill obj = new WindChill();
+
+        // Calculate wind chill
+        double windChill = obj.calculateWindChill(temprature, windSpeed);
+
+        // Display wind chill value
         System.out.println(windChill);
-        
     }
 }
