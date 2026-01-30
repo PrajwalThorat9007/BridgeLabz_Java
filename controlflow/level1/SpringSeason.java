1dbed0f01de673e2677519c44207c63155c72e98
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:02:36 +0530

    Update SpringSeason.java

--- controlflow/level1/SpringSeason.java
+++ controlflow/level1/SpringSeason.java
@@ -6,14 +6,20 @@ Spring Season is from March 20 to June 20
 
 import java.util.Scanner;
 
-class SpringSeason{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int month=sc.nextInt();
-        int date=sc.nextInt();
-        if((month==3 && date>=20)||month==4||month==5||(month==6 && date<=20)){
+class SpringSeason {
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
+
+        // Read month and date
+        int month = sc.nextInt();
+        int date = sc.nextInt();
+
+        // Check if the given date falls in Spring season
+        if ((month == 3 && date >= 20) || month == 4 || month == 5 || (month == 6 && date <= 20)) {
             System.out.println("It's Spring season");
-        }else{
+        } else {
             System.out.println("It's not Spring season");
         }
     }
