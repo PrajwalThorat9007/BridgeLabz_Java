62931765bc8e8fe232e5436c03718c4df4b8b8ec
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 10:40:19 +0530

    Update MultiplicationTableArray.java

--- arrays/level1/MultiplicationTableArray.java
+++ arrays/level1/MultiplicationTableArray.java
@@ -9,15 +9,22 @@ import java.util.Scanner;
 
 class MultiplicationTableArray {
     public static void main(String[] args) {
+
+        // Create Scanner object to take input
         Scanner sc = new Scanner(System.in);
 
+        // Read the number for multiplication table
         int number = sc.nextInt();
+
+        // Array to store multiplication table results
         int[] table = new int[10];
 
+        // Store multiplication results in array
         for (int i = 1; i <= 10; i++) {
             table[i - 1] = number * i;
         }
 
+        // Print the multiplication table using array values
         for (int i = 1; i <= 10; i++) {
             System.out.println(number + " * " + i + " = " + table[i - 1]);
         }
