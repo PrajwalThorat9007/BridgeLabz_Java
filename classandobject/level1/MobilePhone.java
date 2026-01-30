df425f3ea15894703784e304ed95161cc2f01411
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 12:24:05 +0530

    Update MobilePhone.java

--- classandobject/level1/MobilePhone.java
+++ classandobject/level1/MobilePhone.java
@@ -2,27 +2,37 @@
 Program to Handle Mobile Phone Details
 Problem Statement: Create a MobilePhone class with attributes brand, model, and price. Add a method to display all the details of the phone. The MobilePhone class uses attributes to store the phone's characteristics. The method is used to retrieve and display this information for each object.
  */
-class MobilePhone{
+// Class representing a Mobile Phone
+class MobilePhone {
+
+    // Private data members to ensure encapsulation
     private String brand;
     private String model;
     private int price;
     
-    public void setData(String brand,String model,int price){
-        this.brand=brand;
-        this.model=model;
-        this.price=price;
+    // Method to set mobile phone details
+    public void setData(String brand, String model, int price){
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
     }
+
+    // Method to display mobile phone details
     public void getData(){
-        System.out.println("Brand Name: "+brand);
-        System.out.println("Model Name: "+model);
-        System.out.println("Price is: "+price);
+        System.out.println("Brand Name: " + brand);
+        System.out.println("Model Name: " + model);
+        System.out.println("Price is: " + price);
     }
     
     public static void main(String[] args){
-      MobilePhone ph1=new MobilePhone();
-      ph1.setData("iPhone","14 pro max",100000);
+
+        // Create MobilePhone object
+        MobilePhone ph1 = new MobilePhone();
+
+        // Set phone data
+        ph1.setData("iPhone", "14 pro max", 100000);
     
+        // Display phone data
         ph1.getData();  
     }
-    
 }
