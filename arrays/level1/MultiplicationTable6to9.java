43234bd8c657bac305ae8e74b519b6fd86ad3e04
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 10:39:46 +0530

    Update MultiplicationTable6to9.java

--- arrays/level1/MultiplicationTable6to9.java
+++ arrays/level1/MultiplicationTable6to9.java
@@ -10,21 +10,33 @@ import java.util.Scanner;
 
 class MultiplicationTable6to9 {
     public static void main(String[] args) {
+
+        // Create Scanner object to take input
         Scanner sc = new Scanner(System.in);
 
+        // Read the number for multiplication table
         int number = sc.nextInt();
+
+        // Array to store multiplication results for 6 to 9
         int[] multiplicationResult = new int[4]; // for 6,7,8,9
 
+        // Index to store results in array
         int index = 0;
+
+        // Calculate multiplication results from 6 to 9
         for (int i = 6; i <= 9; i++) {
             multiplicationResult[index] = number * i;
             index++;
         }
 
+        // Reset index for printing results
         index = 0;
+
+        // Print multiplication table from 6 to 9
         for (int i = 6; i <= 9; i++) {
             System.out.println(number + " * " + i + " = " + multiplicationResult[index]);
             index++;
         }
     }
 }
+
