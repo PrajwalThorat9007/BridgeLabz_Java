c4d09688cffb0328bdf573926a85283a22c9deb4
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:24:16 +0530

    Update AthleteRounds.java

--- javaprogrammingelements/level2/AthleteRounds.java
+++ javaprogrammingelements/level2/AthleteRounds.java
@@ -9,19 +9,28 @@ import java.util.Scanner;
 
 class AthleteRounds {
     public static void main(String[] args) {
+
+        // Create Scanner object to take input
         Scanner sc = new Scanner(System.in);
 
+        // Read lengths of three sides of the triangular track
         double side1 = sc.nextDouble();
         double side2 = sc.nextDouble();
         double side3 = sc.nextDouble();
 
+        // Calculate perimeter of the triangular track
         double perimeter = side1 + side2 + side3;
-        double totalDistance = 5000; // 5 km in meters
 
+        // Total distance to be covered (5 km in meters)
+        double totalDistance = 5000;
+
+        // Calculate number of rounds required to complete 5 km
         double rounds = totalDistance / perimeter;
 
+        // Display the number of rounds
         System.out.println(
-            "The total number of rounds the athlete will run is " + rounds + " to complete 5 km"
+            "The total number of rounds the athlete will run is " + rounds +
+            " to complete 5 km"
         );
     }
 }
