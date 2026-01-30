e9a8c0fc150332beb2f92c4457be735d975e239b
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:36:40 +0530

    Update NoHandshake.java

--- methods/level1/NoHandshake.java
+++ methods/level1/NoHandshake.java
@@ -5,16 +5,27 @@ Use the combination = (n * (n - 1)) / 2 formula to calculate the maximum number 
 Write a method to use the combination formulae to calculate the number of handshakes
  */
 import java.util.Scanner;
-class NoHandshake{
-    void NoOfhandshake(int num){
-        int Handshake=(num*(num-1))/2;
+
+class NoHandshake {
+
+    // Method to calculate number of handshakes
+    void NoOfhandshake(int num) {
+        int Handshake = (num * (num - 1)) / 2;
         System.out.println(Handshake);
     }
-    public static void main(String[] args){
-        NoHandshake obj=new NoHandshake();
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
 
+    public static void main(String[] args) {
+
+        // Create object of NoHandshake class
+        NoHandshake obj = new NoHandshake();
+
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
+
+        // Read number of people
+        int num = sc.nextInt();
+
+        // Call method to calculate handshakes
         obj.NoOfhandshake(num);
     }
 }
