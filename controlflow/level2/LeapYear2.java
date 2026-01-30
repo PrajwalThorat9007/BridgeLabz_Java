c4a6251d3dfa5f742c6bb81b7bf49b1321325248
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:08:15 +0530

    Update LeapYear2.java

--- controlflow/level2/LeapYear2.java
+++ controlflow/level2/LeapYear2.java
@@ -6,10 +6,14 @@ import java.util.Scanner;
 
 class LeapYear2 {
     public static void main(String[] args) {
+
+        // Create Scanner object to take input
         Scanner sc = new Scanner(System.in);
 
+        // Read the year
         int year = sc.nextInt();
 
+        // Check if the year is a leap year (Gregorian calendar rules)
         if (year >= 1582 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
             System.out.println("Year is a Leap Year");
         } else {
