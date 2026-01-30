0649875d9c820afab1c9ec392d7a11ea7b0455f4
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:28:51 +0530

    Update SwapNumbers.java

--- javaprogrammingelements/level2/SwapNumbers.java
+++ javaprogrammingelements/level2/SwapNumbers.java
@@ -12,15 +12,20 @@ import java.util.Scanner;
 
 class SwapNumbers {
     public static void main(String[] args) {
+
+        // Create Scanner object to take input
         Scanner sc = new Scanner(System.in);
 
+        // Read two numbers
         int number1 = sc.nextInt();
         int number2 = sc.nextInt();
 		
+        // Swap the two numbers using a temporary variable
         int temp = number1;
         number1 = number2;
         number2 = temp;
 
+        // Display the swapped numbers
         System.out.println(
             "The swapped numbers are " + number1 + " and " + number2
         );
