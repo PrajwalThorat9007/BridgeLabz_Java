e8218349860dc7ca668fc792d9841a4d59f3595d
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:00:13 +0530

    Implement countdown from user-defined starting value
    Added countdown functionality using a for loop.

--- controlflow/level1/RocketLaunch2.java
+++ controlflow/level1/RocketLaunch2.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 
 class RocketLaunch2 {
     public static void main(String[] args) {
+
+        // Create Scanner object to take input
         Scanner sc = new Scanner(System.in);
 
+        // Read the starting counter value
         int counter = sc.nextInt();
 
+        // Countdown using for loop
         for (int i = counter; i >= 1; i--) {
             System.out.println(i);
         }
