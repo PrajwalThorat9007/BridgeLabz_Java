8e9a9ecbf0dd7559b9961df8c2e550f7fe55e479
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:25:39 +0530

    Update DoubleOpt.java

--- javaprogrammingelements/level2/DoubleOpt.java
+++ javaprogrammingelements/level2/DoubleOpt.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 
 class DoubleOpt {
     public static void main(String[] args) {
+
+        // Create Scanner object to take input
         Scanner sc = new Scanner(System.in);
 
+        // Read three double values
         double a = sc.nextDouble();
         double b = sc.nextDouble();
         double c = sc.nextDouble();
 
+        // Perform different double arithmetic operations
         double result1 = a + b * c;
         double result2 = a * b + c;
         double result3 = c + a / b;
         double result4 = a % b + c;
 
+        // Display results of all operations
         System.out.println(
-            "The results of Double Operations are " +result1 + ", " +result2 + ", " +result3 + ", and " +
+            "The results of Double Operations are " + result1 + ", " + result2 + ", " + result3 + ", and " +
             result4
         );
     }
