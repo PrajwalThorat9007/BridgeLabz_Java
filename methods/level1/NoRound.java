ef6e3dbc2f6fe79174626be41e0a013124c6e42f
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:37:15 +0530

    Update NoRound.java

--- methods/level1/NoRound.java
+++ methods/level1/NoRound.java
@@ -5,25 +5,40 @@ The perimeter of a triangle is the addition of all sides and rounds is distance/
 Write a Method to compute the number of rounds user needs to do to complete 5km run
  */
 import java.util.Scanner;
-class NoRound{
 
-    void Rounds(int side1,int side2,int side3){
-        int round=side1+side2+side3;
-        int noRound=round/5;
-        if(noRound<=0){
+class NoRound {
+
+    // Method to calculate number of rounds based on track sides
+    void Rounds(int side1, int side2, int side3) {
+
+        // Calculate perimeter of the track
+        int round = side1 + side2 + side3;
+
+        // Calculate number of rounds for 5 km
+        int noRound = round / 5;
+
+        // Check if running is required
+        if (noRound <= 0) {
             System.out.println("No need to Run");
-        }
-        else{
+        } else {
             System.out.println(noRound);
         }
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int side1=sc.nextInt();
-        int side2=sc.nextInt();
-        int side3=sc.nextInt();
 
-        NoRound obj=new NoRound();
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
+
+        // Read lengths of three sides
+        int side1 = sc.nextInt();
+        int side2 = sc.nextInt();
+        int side3 = sc.nextInt();
+
+        // Create object of NoRound class
+        NoRound obj = new NoRound();
+
+        // Call method to calculate number of rounds
         obj.Rounds(side1, side2, side3);
     }
 }
