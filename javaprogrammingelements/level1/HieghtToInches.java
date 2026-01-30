165f7cc35b5982dd4b4e74cf06680e98f2aed39e
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:18:28 +0530

    Update HieghtToInches.java

--- javaprogrammingelements/level1/HieghtToInches.java
+++ javaprogrammingelements/level1/HieghtToInches.java
@@ -6,13 +6,25 @@ O/P => Your Height in cm is ___ while in feet is ___ and inches is ___
 */
 import java.util.Scanner;
 
-class HieghtToInches{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int Height=sc.nextInt();
-        double inch=Height/2.54;
-        double feet=inch/12;
+class HieghtToInches {
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
+
+        // Read height in centimeters
+        int Height = sc.nextInt();
+
+        // Convert height from centimeters to inches
+        double inch = Height / 2.54;
+
+        // Convert height from inches to feet
+        double feet = inch / 12;
         
-        System.out.printf("Your Height in cm is %d while in feet is %.2f and inches is %.2f",Height,feet,inch);
+        // Display height in centimeters, feet, and inches
+        System.out.printf(
+            "Your Height in cm is %d while in feet is %.2f and inches is %.2f",
+            Height, feet, inch
+        );
     }
 }
