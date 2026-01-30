90580b4834bd6ed56fe1537204c430206c8064db
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:49:15 +0530

    Update FootballTeamHeight.java

--- methods/level3/FootballTeamHeight.java
+++ methods/level3/FootballTeamHeight.java
@@ -13,6 +13,7 @@ import java.util.Random;
 
 class FootballTeamHeight {
 
+    // Method to calculate sum of heights
     public static int findSum(int[] heights) {
         int sum = 0;
         for (int h : heights) {
@@ -21,11 +22,13 @@ class FootballTeamHeight {
         return sum;
     }
 
+    // Method to calculate mean height
     public static double findMean(int[] heights) {
         int sum = findSum(heights);
         return (double) sum / heights.length;
     }
 
+    // Method to find the shortest height
     public static int findShortest(int[] heights) {
         int min = heights[0];
         for (int h : heights) {
@@ -36,6 +39,7 @@ class FootballTeamHeight {
         return min;
     }
 
+    // Method to find the tallest height
     public static int findTallest(int[] heights) {
         int max = heights[0];
         for (int h : heights) {
@@ -48,25 +52,33 @@ class FootballTeamHeight {
 
     public static void main(String[] args) {
 
+        // Array to store heights of football team players
         int[] heights = new int[11];
+
+        // Random object to generate heights
         Random rand = new Random();
 
+        // Generate random heights between 150 and 250 cm
         for (int i = 0; i < heights.length; i++) {
             heights[i] = rand.nextInt(101) + 150;
         }
 
+        // Display all player heights
         System.out.println("Player Heights (cm):");
         for (int h : heights) {
             System.out.print(h + " ");
         }
         System.out.println();
 
+        // Calculate shortest, tallest, and mean height
         int shortest = findShortest(heights);
         int tallest = findTallest(heights);
         double mean = findMean(heights);
 
+        // Display calculated results
         System.out.println("Shortest Height: " + shortest + " cm");
         System.out.println("Tallest Height: " + tallest + " cm");
         System.out.println("Mean Height: " + mean + " cm");
     }
 }
+
