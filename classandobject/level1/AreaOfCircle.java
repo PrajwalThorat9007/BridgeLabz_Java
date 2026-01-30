8c75c66b2833a3088993031fc1f4d5cb169129ac
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 12:21:25 +0530

    Update AreaOfCircle.java

--- classandobject/level1/AreaOfCircle.java
+++ classandobject/level1/AreaOfCircle.java
@@ -4,27 +4,39 @@ Problem Statement: Write a program to create a Circle class with an attribute ra
  */
 import java.util.Scanner;
 
-class AreaOfCircle{
+class AreaOfCircle {
+
+    // Private data member for radius (not directly used)
     private int radius;
     
+    // Method to calculate area of a circle
     public double Area(int radius){
-        double area=Math.PI*Math.pow(radius,2);
+        double area = Math.PI * Math.pow(radius, 2);
         return area;
     }
     
+    // Method to calculate circumference of a circle
     public double Circumference(int radius){
-        double circumference=2*Math.PI*radius;
+        double circumference = 2 * Math.PI * radius;
         return circumference;
     }
     
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int radius=sc.nextInt();
         
-        AreaOfCircle obj=new AreaOfCircle();
-        double A=obj.Area(radius);
-        double C=obj.Circumference(radius);
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
+
+        // Read radius of the circle
+        int radius = sc.nextInt();
         
-        System.out.println("Area of Circle "+A+" and circumference is "+C);
+        // Create object of AreaOfCircle class
+        AreaOfCircle obj = new AreaOfCircle();
+
+        // Calculate area and circumference
+        double A = obj.Area(radius);
+        double C = obj.Circumference(radius);
+        
+        // Display the results
+        System.out.println("Area of Circle " + A + " and circumference is " + C);
     }
 }
