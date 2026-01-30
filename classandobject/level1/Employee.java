f48454d7461cde00a74f6f859bc47b70e2202e00
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 12:22:43 +0530

    Update Employee.java

--- classandobject/level1/Employee.java
+++ classandobject/level1/Employee.java
@@ -3,29 +3,45 @@ Program to Display Employee Details
 Problem Statement: Write a program to create an Employee class with attributes name, id, and salary. Add a method to display the details.
  */
 import java.util.Scanner;
-class Employee{
+
+// Class representing an Employee
+class Employee {
+
+    // Private data members to ensure encapsulation
     private String name;
     private int id;
     private int salary;
     
-    public void setData(String name,int id,int salary){
-        this.name=name;
-        this.id=id;
-        this.salary=salary;
+    // Method to set employee details
+    public void setData(String name, int id, int salary){
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
     }
+
+    // Method to display employee details
     public void getData(){
-        System.out.println("Employee name is "+name+" id is "+id+" and salary is "+salary);
+        System.out.println("Employee name is " + name + " id is " + id + " and salary is " + salary);
     }
     
     public static void main(String[] args){
-        Employee emp1=new Employee();
-        Scanner sc=new Scanner(System.in);
+
+        // Create Employee object
+        Employee emp1 = new Employee();
+
+        // Create Scanner object to take input
+        Scanner sc = new Scanner(System.in);
         
-        String name=sc.next();
-        int id=345;
-        int salary=1500000;
+        // Read employee name
+        String name = sc.next();
+
+        // Assign fixed values for id and salary
+        int id = 345;
+        int salary = 1500000;
         
-        emp1.setData(name,id,salary);
+        // Set and display employee data
+        emp1.setData(name, id, salary);
         emp1.getData();
     }
 }
+
