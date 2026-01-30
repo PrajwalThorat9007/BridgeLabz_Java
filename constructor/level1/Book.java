f2c635c33956b4dc30ecfc9549671d3961cf62cb
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 12:28:55 +0530

    Update Book.java

--- constructor/level1/Book.java
+++ constructor/level1/Book.java
@@ -1,24 +1,29 @@
 /*
 Create a Book class with attributes title, author, and price. Provide both default and parameterized constructors.
  */
+// Class representing a Book
 class Book {
 
+    // Private data members to ensure encapsulation
     private String title;
     private String author;
     private double price;
 
+    // Default constructor initializing default values
     public Book() {
         title = "Unknown";
         author = "Unknown";
         price = 0.0;
     }
 
+    // Parameterized constructor to initialize book details
     public Book(String title, String author, double price) {
         this.title = title;
         this.author = author;
         this.price = price;
     }
 
+    // Method to display book information
     public void displayBook() {
         System.out.println("Title: " + title);
         System.out.println("Author: " + author);
@@ -27,12 +32,17 @@ class Book {
 
     public static void main(String[] args) {
 
+        // Create Book object using default constructor
         Book b1 = new Book();
+
+        // Create Book object using parameterized constructor
         Book b2 = new Book("Clean Code", "Robert C. Martin", 450.0);
 
+        // Display details of default constructor object
         System.out.println("Default Constructor Output:");
         b1.displayBook();
 
+        // Display details of parameterized constructor object
         System.out.println("\nParameterized Constructor Output:");
         b2.displayBook();
     }
