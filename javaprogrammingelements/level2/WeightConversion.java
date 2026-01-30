8564cf82da2ab8cc690903c7382ed741ddb64ffc
Prajwal Sanjay Thorat <145048292+PrajwalThorat9007@users.noreply.github.com>
2026-01-30 11:31:00 +0530

    Update WeightConversion.java

--- javaprogrammingelements/level2/WeightConversion.java
+++ javaprogrammingelements/level2/WeightConversion.java
@@ -8,15 +8,21 @@ import java.util.Scanner;
 
 class WeightConversion {
     public static void main(String[] args) {
+
+        // Create Scanner object to take input
         Scanner sc = new Scanner(System.in);
 
+        // Read weight in pounds
         double pound = sc.nextDouble();
 
+        // Convert weight from pounds to kilograms
         double kilogram = pound / 2.2;
 
+        // Display weight in pounds and kilograms
         System.out.println(
             "The weight of the person in pound is " + pound +
             " and in kg is " + kilogram
         );
     }
 }
+
